85b043e63c2680abe954cab71dc3e8ca9c763152
vasu4651 <vasu4651@gmail.com>
2022-01-20 22:15:09 +0530

    2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let lightBtnOn = document.querySelector('#lightBtnOn');
 let lightBtnOff = document.querySelector('#lightBtnOff');
 let fanBtnOn = document.querySelector('#fanBtnOn');
 let fanBtnOff = document.querySelector('#fanBtnOff');
-let val1 = 0;
 
 function switchlightOn(data){
     console.log('Light switched on');
@@ -11,6 +10,16 @@ function switchlightOn(data){
 
 function switchlightOff(data){
     console.log('Light switched off');
+    console.log(data);
+}
+
+function switchfanOn(data){
+    console.log('Fan switched on');
+    console.log(data);
+}
+
+function switchfanOff(data){
+    console.log('Fan switched off');
     console.log(data);
 }
 
@@ -33,5 +42,27 @@ lightBtnOff.addEventListener('click', ()=>{
         .then(data => {
             if(data!=0)
                 switchlightOff(data);
+        })
+});
+
+fanBtnOn.addEventListener('click', ()=>{
+    fetch('https://api.thingspeak.com/update?api_key=WWO63DEOP62JURH0&field2=1')
+        .then(res => {
+            return res.text();
+        })
+        .then(data => {
+            if(data!=0)
+                switchfanOn(data);
+        })
+});
+
+fanBtnOff.addEventListener('click', ()=>{
+    fetch('https://api.thingspeak.com/update?api_key=WWO63DEOP62JURH0&field2=0')
+        .then(res => {
+            return res.text();
+        })
+        .then(data => {
+            if(data!=0)
+                switchfanOff(data);
         })
 });
